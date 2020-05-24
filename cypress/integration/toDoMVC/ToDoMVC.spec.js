2b860ac3aaac04c5d66040970663196457ec34e2
maciejkucharski <maciej.kucharski@senti1.com>
2020-05-24 09:04:27 +0200

    added apps for second part, fix

---
+++ cypress/integration/toDoMVC/ToDoMVC.spec.js
@@ -0,0 +1,46 @@
+import {addNewItem} from "../../support/utils";
+
+describe('ToDo App', () => {
+  context('When page initially loaded', () => {
+    beforeEach(() => {
+      cy.visit('');
+    });
+
+    it('header of the page should be visible and have proper name', () => {
+      cy.get('.header h1').should('be.visible').and('contain.text', 'todos')
+    });
+
+    it('input should have proper placeholder', () => {
+      cy.get('input').should('have.attr', 'placeholder', 'What needs to be done?')
+    })
+  });
+
+  context('When added new item', () => {
+    beforeEach(() => {
+      cy.visit('');
+    });
+
+    it('should be added only one item', () => {
+      cy.get('input.new-todo').type('Hello!').type('{enter}')
+
+      cy.get('.todo-list li').should('have.length', 1)
+    });
+
+    it('should add 3 items with proper order ', () => {
+        const firstItem = "First Item";
+        const secondItem = "Second Item";
+        const thirdItem = "Third Item";
+
+        addNewItem(firstItem);
+        addNewItem(secondItem);
+        addNewItem(thirdItem);
+
+        cy.get('.todo-list li').eq(0).should('contain.text', firstItem);
+        cy.get('.todo-list li').eq(1).should('contain.text', secondItem);
+        cy.get('.todo-list li').eq(2).should('contain.text', thirdItem);
+
+    })
+  });
+
+
+});
